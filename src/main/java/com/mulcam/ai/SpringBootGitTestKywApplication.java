adc1af8ea3e1c69bb4d07aff1a258b841400f1d2
yewon <yewon@192.168.0.3>
2021-03-27 15:20:33 +0900

    [김예원]첫프로젝트구성

---
+++ src/main/java/com/mulcam/ai/SpringBootGitTestKywApplication.java
@@ -0,0 +1,13 @@
+package com.mulcam.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitTestKywApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitTestKywApplication.class, args);
+	}
+
+}
